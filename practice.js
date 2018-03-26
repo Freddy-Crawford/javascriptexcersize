d98b805d4ef88f3a6e19db81a5fd684900f27750
freddy-crawford <freddy_crawford@yahoo.com>
2018-03-26 17:50:24 -0600

    added javascript practice for bill calculator.

---
+++ practice.js
@@ -0,0 +1,69 @@
+//alert("hello world");
+
+document.write ("hello World");
+//this will displaysecret message to the console
+
+console.log("secret message");
+
+//
+var fireEngineColor=("red")
+console.log(fireEngineColor);
+
+// var numberOfTruck=(25);
+// var numberOfCars=(11);
+// console.log(numberOfTruck+numberOfCars);
+
+
+// var numberOfEmails=(17);
+// var numberOfTexts=(25);
+// var numberOfLetters=(18);
+
+// console.log(numberOfEmails*numberOfLetters);
+
+
+// var firstName=('freddy ');
+// var lastName=('crawford ');
+
+// var fullName=(firstName + lastName);
+// console.log(fullName);
+
+// var firstName=('freddy ');
+// var lastName=('crawford ');
+// var fullName=(firstName+lastName);
+
+
+// var preTip=27;
+// var tipPercentage=.15;
+
+// var totalBill='your total bill will be' + (preTip *tipPercentage +preTip );
+// console.log(totalBill);
+
+
+// var helloWorld= function(){
+// console.log("hello world");
+// }
+// helloWorld();
+
+
+// var fullName=function(firstName,lastName){
+// var name=(firstName+lastName);
+// return(name);
+// }
+
+// console.log(fullName("greg", "smith"));
+
+
+
+var temperature=(4)
+
+if (temperature<=50&&temperature>30){
+	console.log("put on coat");
+}
+
+else if(temperature<=30&&temperature>0){
+	console.log("put on a coat and hat")
+}
+else if(temperature<=0){
+	console.log("stay inside")}
+
+
